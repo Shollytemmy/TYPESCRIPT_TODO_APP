d647bcc041c69705f338b705fbd494b446e0598e
Bunyameen <bunyameen@gmail.com>
2022-10-29 08:36:13 +0100

    added inputFeild component & styles

---
+++ src/components/InputField.tsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+const InputField = () => {
+  return (
+    <form className='input'>
+        <input type="text" name="" id="" placeholder='Type Here ...' className='input__box' />
+        <button type='submit' className='input__submit'>Go</button>
+    </form>
+  )
+}
+
+export default InputField
