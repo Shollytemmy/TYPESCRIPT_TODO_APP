b753e6a0d090772826afdd82fc1bc99b96e1d36a
Bunyameen <bunyameen@gmail.com>
2022-10-29 10:34:01 +0100

    added todoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todos } from './Model';
 
 function App() {
+  const [todo, setTodo] = useState<string>("")
+  const [todos, setTodos] = useState<Todos[]>([])
+
+
+  const addTodo = (e:React.FormEvent) =>{
+    e.preventDefault()
+
+    if(todo){
+      setTodos([...todos, {id: Date.now(), todo: todo, completed: false}])
+     
+
+    }
+
+     setTodo("")
+
+
+    
+    
+
+  }
   return (
     <div className="App">
       <span className='heading'>Taskify</span>
-      <InputField />
+      <InputField todo = {todo} setTodo = {setTodo} addTodo = {addTodo}  />
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,24 @@
+import React from 'react'
 import './style.css'
 
-const InputField = () => {
+interface Props {
+    todo: string,
+    setTodo: React.Dispatch<React.SetStateAction<string>>,
+    addTodo: (e: React.FormEvent) => void
+}
+
+const InputField = ({todo, setTodo, addTodo}: Props) => {
   return (
-    <form className='input'>
-        <input type="text" name="" id="" placeholder='Type Here ...' className='input__box' />
+    <form className='input' onSubmit={addTodo}>
+        <input 
+        type="text" 
+        name="" 
+        id=""
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+         placeholder='Type Here ...'
+          className='input__box'
+          />
         <button type='submit' className='input__submit'>Go</button>
     </form>
   )
