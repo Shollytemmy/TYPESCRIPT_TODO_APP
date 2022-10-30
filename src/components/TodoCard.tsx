a18a4da3ea468b5836f787ae645d4aca6cb94a9e
Bunyameen <bunyameen@gmail.com>
2022-10-30 13:02:14 +0100

    Edit todo functionality added

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Todos } from '../Model'
 import {AiFillEdit} from 'react-icons/ai'
 import {AiFillDelete} from 'react-icons/ai'
@@ -12,6 +12,8 @@ type Props = {
 
 
 const TodoCard = ({todo, setTodos, todos}: Props) => {
+  const [edit, setEdit] = useState<boolean>(false)
+  const [editTodo, setEditTodo] = useState<string>(todo.todo)
 
   const handleDone = (id: number) => {
     setTodos(todos.map((todo) => todo.id === id ? {...todo, completed: !todo.completed}: todo))
@@ -23,12 +25,29 @@ const TodoCard = ({todo, setTodos, todos}: Props) => {
     setTodos(todos.filter((todo) => todo.id !== id))
 
   }
-  return (
-    <form className="todos__card">
 
-        <span className="card__text" style={todo.completed ? {textDecoration: "line-through"}: undefined}>{todo.todo}</span>
+  
+  return (
+    <form className="todos__card" onSubmit={(e) => handleEdit(e, todo.id)}>
+
+      {
+        edit ? (
+          <input type="text" value={editTodo} onChange={(e) => setEditTodo(e.target.value)} className='card__input'/>
+        ) : (
+          <span className="card__text" style={todo.completed ? {textDecoration: "line-through"}: undefined}>{todo.todo}</span>
+
+        )
+      }
+
+        
         <div>
-            <span className="icon">{<AiFillEdit />}</span>
+            <span className="icon" onClick={() => {
+              if(!edit && !todo.completed){
+                setEdit(!edit)
+              }
+              
+
+            }}>{<AiFillEdit />}</span>
             <span className="icon" onClick={() => handleDone(todo.id)}>{<MdDone />}</span>
             <span className="icon" onClick={() => handleDelete(todo.id)}>{<AiFillDelete />}</span>
         </div>
